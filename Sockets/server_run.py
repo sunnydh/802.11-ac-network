c36ec5bc838d10a6cc301ba1baba4e1d93a63b66
sunnydh <dhamnanisunny.1402@gmail.com>
2016-04-09 13:58:22 +0530

    compute average file added

--- Sockets/server_run.py
+++ Sockets/server_run.py
@@ -19,38 +19,38 @@ def main(run):
 	# 	run = int(f.readline())
 
 
-	with open("log.txt","a+") as f:
+	with open("/home/susan/Desktop/BTP/Server/log.txt","a+") as f:
 		f.write("Start " + str(run) + "\n")
 
 	print "Start ", run
 
-	f_out = open("tcpdumpSTDOUT", "w")
-	f_err = open("tcpdumpSTDERR", "w")
+	f_out = open("/home/susan/Desktop/BTP/Server/tcpdumpSTDOUT", "w")
+	f_err = open("/home/susan/Desktop/BTP/Server/tcpdumpSTDERR", "w")
 
-	cmd = "sudo tcpdump -n -s0 -w /home/susan/Desktop/BTP/Server/2.4_20/trace"+ str(run) +".pcap -i wlan2"
+	cmd = "sudo tcpdump -n -s0 -w /home/susan/Desktop/BTP/Server/5_80/trace"+ str(run) +".pcap -i wlan1"
 	ip = "192.168.1.2"
 	port = "3300"
 
 	first = subprocess.Popen(shlex.split(cmd), stderr=f_err, stdout=f_out)
 	time.sleep(2)	
-	second = subprocess.Popen(['/home/susan/Desktop/BTP/802.11-ac-network/Sockets/server.o', ip, port, '2.4_20', str(run)])
+	second = subprocess.Popen(['/home/susan/Desktop/BTP/802.11-ac-network/Sockets/server.o', ip, port, '5_80', str(run)])
 	
 	second.wait()
 	os.system("sudo kill %s" % (first.pid, ))
 	f_out.close()
 	f_err.close()
 
-	with open("log.txt","a+") as f:
+	with open("/home/susan/Desktop/BTP/Server/log.txt","a+") as f:
 		f.write("Complete " + str(run) + "\n")
 
 	# run += 1
 	# with open("run.txt","w") as f:
 	# 	f.write(str(run))
 
-	print "End ", run
+	print "End ", run, "\n------------------------------------------------"
 
 if __name__ == '__main__':
 
-	for i range(1,21):
+	for i in range(1,11):
 		main(i)
-		time.sleep(10)
+		time.sleep(100)
